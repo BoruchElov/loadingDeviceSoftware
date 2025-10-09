68043aee0de3d272f3e1c714e275572b4289b671
BoruchElov <malyutaborisdim@gmail.com>
2025-10-09 12:41:42 +0700

    Подготовил основу для нового комбобокса

---
+++ src/main/java/org/example/loadingdevicesoftware/logicAndSettingsOfInterface/SimpleComboBox.java
@@ -0,0 +1,15 @@
+package org.example.loadingdevicesoftware.logicAndSettingsOfInterface;
+
+import javafx.scene.control.ComboBox;
+
+public class SimpleComboBox extends ComboBox implements Changeable{
+    @Override
+    public void changePosition(int position) {
+
+    }
+
+    @Override
+    public void setActualStatus(Status status) {
+
+    }
+}
