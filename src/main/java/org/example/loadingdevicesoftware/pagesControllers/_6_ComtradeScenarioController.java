7a123e705fa744a8c8f90bd68f81a16da579dd70
{Boris Maliuta} <{malyutaborisdim@gmail.com}>
2026-02-17 10:33:08 +0700

    Изменения: 1) Добавлена временная задержка при переходе 2) Добавлена черновая версия списка сигналов

--- src/main/java/org/example/loadingdevicesoftware/pagesControllers/_6_ComtradeScenarioController.java
+++ src/main/java/org/example/loadingdevicesoftware/pagesControllers/_6_ComtradeScenarioController.java
@@ -2,15 +2,15 @@ package org.example.loadingdevicesoftware.pagesControllers;
 
 import javafx.animation.PauseTransition;
 import javafx.application.Platform;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import org.example.loadingdevicesoftware.logicAndSettingsOfInterface.*;
@@ -84,45 +84,51 @@ public class _6_ComtradeScenarioController extends ScreensController implements 
     }
 
     private void changeFormState() {
-        if (!formState.get()) {
-            changeConfiguration(new ActionEvent());
-        } else {
-            comtradeButton.setVisible(false);
-            comtradeButton.setManaged(false);
+        Platform.runLater(() -> {
+            PauseTransition pauseTransition = new PauseTransition(javafx.util.Duration.seconds(1));
+            pauseTransition.play();
 
-            leftPane.setVisible(true);
-            leftPane.setManaged(true);
+            pauseTransition.setOnFinished(event -> {
+                if (!formState.get()) {
+                    changeConfiguration(new ActionEvent());
+                } else {
+                    comtradeButton.setVisible(false);
+                    comtradeButton.setManaged(false);
 
-            rightPane.setVisible(true);
-            rightPane.setManaged(true);
-        }
+                    leftPane.setVisible(true);
+                    leftPane.setManaged(true);
+
+                    rightPane.setVisible(true);
+                    rightPane.setManaged(true);
+
+                    setupLeftPane();
+                }
+            });
+        });
     }
 
 
     @Override
     public void changeConfiguration(Event event) {
         formState.set(false);
-
-        leftPane.setBorder(Border.stroke(Paint.valueOf("#000000")));
         leftPane.setPrefWidth(400.);
         leftPane.setPrefHeight(480.);
-        leftPane.backgroundProperty().set(Background.fill(Paint.valueOf("#FFFFFF")));
-        AnchorPane.setTopAnchor(leftPane, 160.);
-        AnchorPane.setLeftAnchor(leftPane, 20.);
-        leftPane.setVisible(false);
-        leftPane.setManaged(false);
-
-        rightPane.setBorder(Border.stroke(Paint.valueOf("#000000")));
         rightPane.setPrefWidth(810.);
         rightPane.setPrefHeight(480.);
-        rightPane.backgroundProperty().set(Background.fill(Paint.valueOf("#FFFFFF")));
         AnchorPane.setTopAnchor(rightPane, 160.);
         AnchorPane.setLeftAnchor(rightPane, 450.);
-        rightPane.setVisible(false);
-        rightPane.setManaged(false);
+        AnchorPane.setTopAnchor(leftPane, 160.);
+        AnchorPane.setLeftAnchor(leftPane, 20.);
 
         for (Node node : anchorPane.getChildren()) {
             switch (node) {
+                case Pane pane when pane == leftPane || pane == rightPane:
+                    pane.setBorder(Border.stroke(Paint.valueOf("#000000")));
+                    pane.backgroundProperty().set(Background.fill(Paint.valueOf("#FFFFFF")));
+                    pane.setVisible(false);
+                    pane.setManaged(false);
+                    break;
+
                 case SimpleButton button when button == comtradeButton:
                     button.setVisible(true);
                     button.setManaged(true);
@@ -142,5 +148,69 @@ public class _6_ComtradeScenarioController extends ScreensController implements 
                     break;
             }
         }
+    }
+
+    public class RowItem {
+        private final BooleanProperty selected = new SimpleBooleanProperty(false);
+        private final StringProperty label = new SimpleStringProperty("");
+
+        public RowItem(String label) {
+            this.label.set(label);}
+
+        public BooleanProperty selectedProperty() { return selected; }
+        public StringProperty labelProperty() { return label; }
+    }
+
+
+    private void setupLeftPane() {
+        ObservableList<RowItem> rowItems = FXCollections.observableArrayList();
+        for (int i = 0; i < 5; i++) {
+            rowItems.add(new RowItem("U" + i));
+        }
+
+        ListView<RowItem> listView = new ListView<>();
+        listView.setItems(rowItems); // ObservableList<RowItem>
+
+        listView.setCellFactory(lv -> new ListCell<>() {
+            private final CheckBox cb = new CheckBox();
+            private final Label text = new Label();
+            private final SimpleTextField tf = new SimpleTextField();
+            private final HBox root = new HBox(10, cb, text, tf);
+
+            // чтобы не “тянуло” строку
+            {
+                HBox.setHgrow(tf, Priority.ALWAYS);
+                text.setFont(FontManager.getFont(FontManager.FontWeight.MEDIUM, FontManager.FontSize.LARGE));
+                tf.setLimits(-100000.,100000., SimpleTextField.numberOfDecimals.THREE);
+                tf.setup("", SimpleTextField.Sizes.MEDIUM, SimpleTextField.typeOfValue.DIGIT);
+                tf.setFont(FontManager.getFont(FontManager.FontWeight.LIGHT, FontManager.FontSize.LARGE));
+            }
+
+            @Override
+            protected void updateItem(RowItem item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty || item == null) {
+                    setGraphic(null);
+                    return;
+                }
+
+                // важно: отвязать старые биндинги
+                cb.selectedProperty().unbindBidirectional(item.selectedProperty());
+
+                // привязки
+                cb.selectedProperty().bindBidirectional(item.selectedProperty());
+                text.textProperty().bind(item.labelProperty());
+
+                setGraphic(root);
+            }
+        });
+        leftPane.getChildren().setAll(listView);
+        AnchorPane.setTopAnchor(listView, 0.0);
+        AnchorPane.setBottomAnchor(listView, 0.0);
+        AnchorPane.setLeftAnchor(listView, 0.0);
+        AnchorPane.setRightAnchor(listView, 0.0);
+
+
     }
 }
